97c5e77aaef6ee5f6366cbd2e474d10e4233cd71
Eduard P. <144683920+bonobix@users.noreply.github.com>
2025-09-16 14:19:00 +0200

    Update streamlit-fetch-wikimedia.py

--- streamlit-fetch-wikimedia.py
+++ streamlit-fetch-wikimedia.py
@@ -13,7 +13,7 @@ from fetch_wikimedia.scripts.filtro_laplaciano import main as laplace_filter
 
 st.set_page_config(page_title="Wikimedia Image Filter", layout="centered")
 
-st.title("🎨 Wikimedia Image Filter")
+st.title("🎨 Wikimedia ImageFilter 🎨")
 st.write("Scarica, filtra per qualità, e seleziona le immagini migliori.")
 
 user_category = st.text_input("🎯 Categoria Wikimedia:", "Paintings by Jan van Goyen")
